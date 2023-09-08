6e399130e18a35cb64e0831a4468587dcc925bc0
Aayush <aayush12arora@gmail.com>
2023-09-08 22:01:33 +0530

    added api for getting classroom details

--- back-end/server.js
+++ back-end/server.js
@@ -1,16 +1,97 @@
 const express=require('express');
 const app=express();
+const mysql = require('mysql');
 
 
 const port=process.env.PORT || 3000;
 
+/*
+
+FOR UNIFORMITY
+KEEP DATABASE NAME AS asur
+*/
+
+const dbName="asur"
+const userName="root"
+const passw="RahulSQL2002"//change this when using on your local machine
+
+
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: userName,
+  password: passw,//my database password
+  database: dbName,
+});
+
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
+
 app.get('/api/getCourseList',(req,res)=>{
-    //to get the course list from the subject table
+  //to get the course list from the subject table
+
+  //CHANGE TABLE NAME
+  const query = "SELECT * FROM subject";
+  connection.query(query, (error, results) => {
+  if (error) throw error;
+  // console.log(results);
+  res.json(results);
+});
 })
 
-app.get('/api/getProfile',(req,res)=>{
-    //to get student details from student table
+app.get('/api/getProfile/:studentID',(req,res)=>{
+  //to get student details from student table
+
+
+  //CHANGE STUDENT TABLE NAME IF NEEDED ACC TO YOUR DB
+  const id= req.params.studentID;
+  const query=`SELECT * from student where Roll_No= ${id}`
+  connection.query(query, (error, results) => {
+      if (error) throw error;
+      // console.log(results);
+      res.json(results);
+    });
+
 })
+
+
+app.get('/api/getClassroomDetails', (req, res) => {
+    // Assuming you have a database connection object named 'db'
+    const coursecode = req.query.coursecode; // Access coursecode from the URL query params
+    console.log('Received coursecode:', coursecode);
+  
+    // Replace 'student_table' with your actual student table name
+    const query = `SELECT * FROM classroom WHERE room_id = (SELECT classroom_id FROM subject WHERE subject_id = '${coursecode}')`;
+  
+    db.query(query, (err, results) => {
+      if (err) {
+        console.error('Error retrieving classroom details:', err);
+        res.status(500).json({ error: 'Internal Server Error' });
+        return;
+      }
+  
+      // Assuming your classroom details are returned as an array of objects
+      const classroomDetails = results[0]; // Get the first (and only) row
+  
+      if (!classroomDetails) {
+        console.error(`Classroom not found for coursecode: ${coursecode}`);
+        res.status(404).json({ error: 'Classroom not found' });
+        return;
+      }
+  
+      res.status(200).json({ classroomDetails });
+    });
+  });
+  
+  
+
+//the above login endpoint will get image from this endpoint
+app.get('/api/image/:studentID',(req,res)=>{
+
+})
+
 
 app.post('/api/markAttendance',(req,res)=>{
     //to insert attendance details into attendance table
